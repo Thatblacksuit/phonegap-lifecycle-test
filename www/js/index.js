2efcbab07961021095ec8fa1b456c8f8914e6195
Thatblacksuit <BAKW1_15@uni.worc.ac.uk>
2017-10-09 13:54:45 +0100

    test2
    test2

--- www/js/index.js
+++ www/js/index.js
@@ -7,7 +7,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 		
 	
 function updateDisplay() {
-	launched.text("Application launched: " + launched_count);
+	launched.innerHTML = "Application launched: " + launched_count;
 	$("#resumed").text("Application paused: " + paused_count);
 	$("#paused").text("Application resumed: " + resumed_count);
 }
